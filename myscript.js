60e577236711637149307234fb470155c77bfd37
sarahelshark <sarahelsh183@gmail.com>
2023-11-20 13:40:32 +0100

    feat add
     start of human coding

---
+++ myscript.js
@@ -0,0 +1,27 @@
+document.writeln('hello world');
+console.log("hello world");
+/*
+Il programma dovrà chiedere all'utente:
+-il numero di chilometri che vuole percorrere
+-l'età del passeggero.
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+Queste sono le regole:
+-il prezzo del biglietto è definito in base ai km (0.21 € al km)
+-va applicato uno sconto del 20% per i minorenni
+-va applicato uno sconto del 40% per gli over 65 (fortunelli).
+
+! L'output del prezzo finale va stampata in pagina assicurandoci sia leggibile da un essere umano (massimo due cifre decimali, per indicare centesimi sul prezzo).
+
+*/
+/*
+TOOLS:
+-2 prompt per ottenere dati 
+-2 variabili per racchiudere questi dati
+-let/const
+-condizionali che si sviluppano a partire dai dati acquisiti
+-writeln oppure un alert dove riporto il risultato
+
+*/
+
+//Chiedo quanti km vuole percorrere l'utente
